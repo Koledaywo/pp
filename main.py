bd4402ea573c5a4db98c9d77ff67f397b32f3066
koleda <y.koleda@complitech.org>
2023-04-13 12:59:12 +0300

    add json decoder error

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from json.decoder import JSONDecodeError
 import requests
+
+#payload = {"name": "User"}
+#response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+#print(response.text)
 
 response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+try:
+    parsed_responce_text = response.json()
+    print(parsed_responce_text)
+except JSONDecodeError:
+    print("Response is not a Json error")
