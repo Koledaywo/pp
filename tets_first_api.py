3502d4f8154e996547d2b83dc9ae2f9488ae83c2
koleda <y.koleda@complitech.org>
2023-04-14 13:44:46 +0300

    first positive test

--- tets_first_api.py
+++ tets_first_api.py
@@ -1,9 +1,15 @@
+import pytest
 import requests
 
 class TestFirstApi:
-    def test_hello_call(self):
+    names = [
+        ("dasha"),
+        ("arson"),
+        ("")
+    ]
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = "Yan"
         data = {'name': name}
 
         response = requests.get(url, params=data)
@@ -11,8 +17,12 @@ class TestFirstApi:
         assert response.status_code == 200, "Wrong response code"
 
         response_dict = response.json()
-        assert "answer" in response_dict, "there is no field 'answer' in the respons"
+        assert "answer" in response_dict, "there is no field 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
+
         actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
